2f9c65e1241c57bb14651dfe1cc69e97d151e0dc
Tloz <ScrimeT@hotmail.fr>
2020-05-16 15:40:09 +0200

    deleted a bunch of useless files

--- tests/testType.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "testType.h"
-
-int main()
-{
-    std::cout << efficiency(Type::Fire, Type::Grass) << std::endl;
-    std::cout << efficiency_text_debug(Type::Fairy, Type::Ghost) << std::endl;
-    return 0;
-}
